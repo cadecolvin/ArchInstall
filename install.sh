8b0688a4c62f0089e21c1f53da9e1d20d92cd2ac
Cade Colvin <cade.colvin@gmail.com>
2017-06-21 09:41:33 -0500

    Redirected output for cleaner script run and added basic logging

--- install.sh
+++ install.sh
@@ -1,38 +1,55 @@
 #!/bin/sh
 
 # Base Packages
-pacman -Sq --noconfirm git
-pacman -Sq --noconfirm base-devel dkms linux-headers
-pacman -Sq --noconfirm cowsay fortune-mod
-pacman -Sq --noconfirm openssh python sudo vim zsh
+echo -n "Installing Base Packages..."
+pacman -Sq --noconfirm git > /dev/null
+pacman -Sq --noconfirm base-devel dkms linux-headers > /dev/null
+pacman -Sq --noconfirm cowsay fortune-mod > /dev/null
+pacman -Sq --noconfirm openssh python sudo vim zsh > /dev/null
+echo "done!"
 
 # For WiFi on Macbook
-pacman -Sq --noconfirm broadcom-wl-dkms
+echo -n "Installing Wifi Packages..."
+pacman -Sq --noconfirm broadcom-wl-dkms > /dev/null
 
 # For Wireless Network Management
-pacman -Sq --noconfirm wpa_supplicant wpa_actiond
-pacman -Sq --noconfirm wifi-menu
+pacman -Sq --noconfirm wpa_supplicant wpa_actiond > /dev/null
+pacman -Sq --noconfirm wifi-menu > /dev/null
+echo "done!"
 
 # For Power Mangement
-pacman -Sq --noconfirm tlp
+echo -n "Installing Power Management..."
+pacman -Sq --noconfirm tlp > /dev/null
 systemctl enable tlp.service
 systemctl enable tlp-sleep.service
 tlp start
+echo "done!"
 
 # Install the GUI stuff
-pacman -Sq --noconfirm xorg xorg-xinit xorg-server 
-pacman -Sq --noconfirm i3-wm i3blocks i3lock i3status
-pacman -Sq --noconfirm rxvt-unicode
+echo -n "Installing xorg..."
+pacman -Sq --noconfirm xorg xorg-xinit xorg-server  > /dev/null
+echo "done!"
+echo -n "Installing i3..."
+pacman -Sq --noconfirm i3-wm i3blocks i3lock i3status > /dev/null
+echo "done!"
+echo -n "Installing URxvt..."
+pacman -Sq --noconfirm rxvt-unicode > /dev/null
+echo "done!"
 
 # Install the fonts that I like
-pacman -Sq --noconfirm powerline
-pacman -Sq --noconfirm ttf-hack
+echo -n "Installing fonts..."
+pacman -Sq --noconfirm powerline > /dev/null
+pacman -Sq --noconfirm ttf-hack > /dev/null
+echo "done!"
 
 # Install Media Codecs
-pacman -Sq --noconfirm exfat-utils fuse-exfat a52dec faac faad2 flac jasper lame libdca libdv gst-libav libmad libmpeg2
-pacman -Sq --noconfirm libtheora libvorbis libxv wavpack x264 xvidcore flashplugin libdvdcss libdvdread libdvdnav
-pacman -Sq --noconfirm gecko-mediaplayer dvd+rw-tools dvdauthor dvgrab
+echo -n "Installing Media Codecs..."
+pacman -Sq --noconfirm exfat-utils fuse-exfat a52dec faac faad2 flac jasper lame libdca libdv gst-libav libmad libmpeg2 > /dev/null
+pacman -Sq --noconfirm libtheora libvorbis libxv wavpack x264 xvidcore flashplugin libdvdcss libdvdread libdvdnav > /dev/null
+pacman -Sq --noconfirm gecko-mediaplayer dvd+rw-tools dvdauthor dvgrab > /dev/null
+echo "done!"
 
+echo "Adding user..."
 # Add myself as a user
 useradd -m -G wheel -s /bin/zsh ccolvin
 
@@ -40,4 +57,5 @@ useradd -m -G wheel -s /bin/zsh ccolvin
 visudo
 
 # Set my password
+echo "Settings user password"
 passwd ccolvin
